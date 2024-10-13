664de723956c7e6fa0abd05a941ccd75e7c4c225
qthequartermasterman <qthequartermasterman@gmail.com>
2024-10-13 10:01:58 -0500

    fix: curriciulum will load for single-digit weeks

--- generate_show/curriculum.py
+++ generate_show/curriculum.py
@@ -5,6 +5,9 @@ import logging
 import bs4
 import httpx
 
+CURRICULUM_LINK = "https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/{week_number}?lang=eng"
+
+
 
 def fetch_curriculum(week_number: int):
     """Fetch the curriculum text for a given week number.
@@ -17,7 +20,8 @@ def fetch_curriculum(week_number: int):
 
     """
     logging.info("Fetching curriculum text")
-    curriculum_link = f"https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/{week_number}?lang=eng"
+    week_number_str = str(week_number).zfill(2)
+    curriculum_link = CURRICULUM_LINK.format(week_number=week_number_str)
     response = httpx.get(curriculum_link)
 
     logging.info("Parsing curriculum text")
