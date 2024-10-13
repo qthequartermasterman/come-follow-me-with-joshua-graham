49f4ef16c64fe9fd2b9f5b11c520d4d6905ad0b3
qthequartermasterman <qthequartermasterman@gmail.com>
2024-10-12 21:25:23 -0500

    feat: add check that to publish dates in the past.

--- generate_show/__main__.py
+++ generate_show/__main__.py
@@ -191,13 +191,19 @@ def determine_publish_date(episode_week: str) -> datetime.datetime:
 
     Returns:
         The publish date for the episode
-
     """
     date_str = episode_week.split("–")[0].strip() + ", 2024"
     date = datetime.datetime.strptime(date_str, "%B %d, %Y")
     publish_date = date - datetime.timedelta(days=1)
     # Set the publish time to 6 PM UTC
     publish_date = publish_date.replace(hour=18, minute=0, second=0, microsecond=0)
+
+    # If the publish date is in the past, then set it to an hour from now
+    if publish_date < datetime.datetime.now(datetime.timezone.utc):
+        publish_date = datetime.datetime.now(
+            datetime.timezone.utc
+        ) + datetime.timedelta(hours=1)
+
     return publish_date
 
 
